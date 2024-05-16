02201942aeb11ee14cd498f4a233f8b9caf90de4
Robin667 <robin.turpin8@gmail.com>
2024-05-16 12:58:04 +0200

    finish createBooking, deleteBooking, infoBooking and check BookingCode

--- actions/actions.py
+++ actions/actions.py
@@ -18,7 +18,6 @@ class CheckIsAvailable(Action):
 
       print(slot_nb_person)
       print(slot_date)
-      print("============")
 
       connection = psycopg2.connect(
         dbname="bqcxetmrbyw8gaxmsxix",
@@ -28,7 +27,7 @@ class CheckIsAvailable(Action):
         port="50013",
       )
       cursor = connection.cursor()
-      cursor.execute("SELECT SUM(nb_person) AS total_nb_person FROM rasa.booking WHERE date = %s;", (slot_date,))
+      cursor.execute("SELECT SUM(number_person) AS total_number_person FROM rasa.booking WHERE date = %s;", (slot_date,))
       total_nb_person = cursor.fetchone()[0]
 
       if total_nb_person is not None:
@@ -37,7 +36,7 @@ class CheckIsAvailable(Action):
         dispatcher.utter_message(f"Nous disposons actuellement de {remaining_places} places disponibles. Souhaitez-vous choisir une nouvelle date ?")
         return [SlotSet("isAvailable", False)]
 
-      dispatcher.utter_message(f"Nous avons suffisamment de places disponibles. Souhaitez-vous confirmer votre réservation ?")
+      dispatcher.utter_message(f"Nous avons suffisamment de places disponibles. Renseigner un nom pour continuer la réservation.")
       return [SlotSet("isAvailable", True)]
 
     except psycopg2.Error as e:
@@ -49,13 +48,11 @@ class CheckIsAvailable(Action):
           cursor.close()
           connection.close()
 
-      return [SlotSet("story_number", False)]
-
 class CreateBooking(Action):
   def name(self) -> Text:
     return "action_create_booking"
 
-  def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[SlotSet]:
+  def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
     try:
       # Génération d'un code de réservation unique
       booking_code = str(uuid.uuid4())
@@ -66,6 +63,7 @@ class CreateBooking(Action):
       slot_phone_nb = tracker.get_slot("slot_phone_nb")
       slot_date = datetime.strptime(tracker.get_slot("slot_date"), "%d/%m/%Y").strftime('%Y-%m-%d')
       slot_time = tracker.get_slot("slot_time")
+      slot_commentary = tracker.get_slot("slot_commentary")
 
       # Connexion à la base de données
       connection = psycopg2.connect(
@@ -78,11 +76,12 @@ class CreateBooking(Action):
       cursor = connection.cursor()
 
       # Insertion des données dans la base de données
-      cursor.execute("INSERT INTO rasa.booking (booking_code, name, number_person, phone_number, date, time) VALUES (%s, %s, %s, %s, %s, %s);",
-        (booking_code, slot_name, slot_nb_person, slot_phone_nb, slot_date, slot_time))
+      cursor.execute("INSERT INTO rasa.booking (booking_code, name, number_person, phone_number, date, time, commentary) VALUES (%s, %s, %s, %s, %s, %s, %s);",
+        (booking_code, slot_name, slot_nb_person, slot_phone_nb, slot_date, slot_time, slot_commentary))
       connection.commit()
 
-      dispatcher.utter_message(f"Votre réservation a été créée avec succès! Votre code de réservation est : {booking_code}")
+      dispatcher.utter_message(f"Votre réservation a été créée avec succès! Votre code de réservation est : {booking_code} \n\nVeuillez garder ce numéro précieusement.\nIl vous sera demandé de le renseigner si vous souhaitez obtenir des infos ou annuler votre réservation.")
+      dispatcher.utter_message(f"\nVoulez-vous un résumé de votre réservation ?")
       return [SlotSet("booking_created", True)]
 
     except psycopg2.Error as e:
@@ -94,6 +93,48 @@ class CreateBooking(Action):
         cursor.close()
         connection.close()
 
+class CheckBookingCode(Action):
+  def name(self) -> Text:
+    return "action_check_booking_code"
+
+  def run(self, dispatcher: CollectingDispatcher,
+          tracker: Tracker,
+          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    try:
+      # Connexion à la base de données
+      connection = psycopg2.connect(
+          dbname="bqcxetmrbyw8gaxmsxix",
+          user="uoj2kgme2r2ouqi2k9xw",
+          password="xWslIJzcfmaYFoVX7XYLITLGIGEbn2",
+          host="bqcxetmrbyw8gaxmsxix-postgresql.services.clever-cloud.com",
+          port="50013"
+      )
+      cursor = connection.cursor()
+
+      # Récupérer l'ID de la réservation à vérifier depuis les slots
+      booking_code = tracker.get_slot("slot_booking_code")
+
+      # Vérifier l'existence de la réservation
+      cursor.execute("SELECT 1 FROM rasa.booking WHERE booking_code = %s;", (booking_code,))
+      result = cursor.fetchone()
+
+      if result:
+        return [SlotSet("isBookingExist", True)]
+      else:
+        dispatcher.utter_message("Le code de réservation est invalide.")
+        return [SlotSet("isBookingExist", False)]
+
+    except psycopg2.Error as e:
+      dispatcher.utter_message("Erreur lors de la vérification du code de réservation :", str(e))
+
+    finally:
+      # Fermer la connexion à la base de données
+      if 'connection' in locals():
+          cursor.close()
+          connection.close()
+
+    return [SlotSet("isBookingExist", False)]
+
 class DeleteBooking(Action):
   def name(self) -> Text:
     return "action_delete_booking"
@@ -102,28 +143,125 @@ class DeleteBooking(Action):
           tracker: Tracker,
           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
     try:
+      # Vérifier si la réservation existe
+      booking_exists = tracker.get_slot("isBookingExist")
+      if not booking_exists:
+        dispatcher.utter_message("La réservation n'existe pas.")
+        return []
+
       # Connexion à la base de données
       connection = psycopg2.connect(
-          dbname="votre_dbname",
-          user="votre_utilisateur",
-          password="votre_mot_de_passe",
-          host="votre_host",
-          port="votre_port"
+          dbname="bqcxetmrbyw8gaxmsxix",
+          user="uoj2kgme2r2ouqi2k9xw",
+          password="xWslIJzcfmaYFoVX7XYLITLGIGEbn2",
+          host="bqcxetmrbyw8gaxmsxix-postgresql.services.clever-cloud.com",
+          port="50013"
       )
       cursor = connection.cursor()
 
       # Récupérer l'ID de la réservation à supprimer depuis les slots
-      booking_id = tracker.get_slot("booking_id")
+      booking_code = tracker.get_slot("slot_booking_code")
 
-      # Supprimer la réservation en fonction de l'ID
-      cursor.execute("DELETE FROM booking WHERE id = %s;", (booking_id,))
+      # Supprimer la réservation en fonction de booking_code
+      cursor.execute("DELETE FROM rasa.booking WHERE booking_code = %s;", (booking_code,))
       connection.commit()
 
       # Envoyer un message de confirmation à l'utilisateur
-      dispatcher.utter_message("La réservation a été supprimée avec succès.")
+      dispatcher.utter_message("La réservation a été annulée avec succès.")
 
     except psycopg2.Error as e:
-      dispatcher.utter_message("Erreur lors de la suppression de la réservation :", str(e))
+      dispatcher.utter_message("Erreur lors de l'annulation de la réservation :", str(e))
+
+    finally:
+      # Fermer la connexion à la base de données
+      if 'connection' in locals():
+          cursor.close()
+          connection.close()
+
+    return []
+
+class UpdateCommentary(Action):
+  def name(self) -> Text:
+    return "action_update_booking_commentary"
+
+  def run(self, dispatcher: CollectingDispatcher,
+          tracker: Tracker,
+          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    try:
+      # Connexion à la base de données
+      connection = psycopg2.connect(
+          dbname="bqcxetmrbyw8gaxmsxix",
+          user="uoj2kgme2r2ouqi2k9xw",
+          password="xWslIJzcfmaYFoVX7XYLITLGIGEbn2",
+          host="bqcxetmrbyw8gaxmsxix-postgresql.services.clever-cloud.com",
+          port="50013"
+      )
+      cursor = connection.cursor()
+
+      # Récupérer l'ID de la réservation et le nouveau commentaire depuis les slots
+      booking_code = tracker.get_slot("slot_booking_code")
+      new_commentary = tracker.get_slot("slot_commentary")
+      print(booking_code)
+      print(new_commentary)
+
+      # Mettre à jour le commentaire de la réservation
+      cursor.execute("UPDATE rasa.booking SET commentary = %s WHERE booking_code = %s;", (new_commentary, booking_code))
+      connection.commit()
+
+      # Envoyer un message de confirmation à l'utilisateur
+      dispatcher.utter_message("Le commentaire de votre réservation a été mis à jour avec succès.")
+
+    except psycopg2.Error as e:
+      dispatcher.utter_message(f"Erreur lors de la mise à jour du commentaire de la réservation : {str(e)}")
+
+    finally:
+      # Fermer la connexion à la base de données
+      if 'connection' in locals():
+        cursor.close()
+        connection.close()
+
+    return []
+
+class GetBookingInfo(Action):
+  def name(self) -> Text:
+    return "action_get_booking_info"
+
+  def run(self, dispatcher: CollectingDispatcher,
+          tracker: Tracker,
+          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    try:
+      # Connexion à la base de données
+      connection = psycopg2.connect(
+          dbname="bqcxetmrbyw8gaxmsxix",
+          user="uoj2kgme2r2ouqi2k9xw",
+          password="xWslIJzcfmaYFoVX7XYLITLGIGEbn2",
+          host="bqcxetmrbyw8gaxmsxix-postgresql.services.clever-cloud.com",
+          port="50013"
+      )
+      cursor = connection.cursor()
+
+      # Récupérer le code de réservation depuis les slots
+      booking_code = tracker.get_slot("slot_booking_code")
+
+      # Rechercher les informations de la réservation en fonction du booking_code
+      cursor.execute("SELECT name, phone_number, number_person, date, time, commentary FROM rasa.booking WHERE booking_code = %s;", (booking_code,))
+      booking_info = cursor.fetchone()
+
+      if booking_info:
+        name, phone_number, number_person, date, time, commentary = booking_info
+        response = (f"Les détails de votre réservation sont les suivants :\n"
+                    f"Nom : {name}\n"
+                    f"Numéro de téléphone : {phone_number}\n"
+                    f"Nombre de personnes : {number_person}\n"
+                    f"Date : {date}\n"
+                    f"Heure : {time}\n"
+                    f"Commentaire : {commentary}")
+        dispatcher.utter_message(response)
+      else:
+        dispatcher.utter_message("Aucune réservation trouvée avec le code fourni.")
+
+    except psycopg2.Error as e:
+      dispatcher.utter_message(f"Erreur lors de la récupération des informations de réservation : {str(e)}")
 
     finally:
       # Fermer la connexion à la base de données
